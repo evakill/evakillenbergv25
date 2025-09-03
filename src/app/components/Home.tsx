283418036e327049f7704579a79fa7b0076c4bff
Eva Killenberg <>
2025-09-02 23:07:12 -0400

    fix readme path

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -25,7 +25,7 @@ export default function Home() {
 
     return <>
         <TreeGrid treeNode={node} />
-        {node.key == "" && <p className="text-base fixed bottom-50 left-15 w-[200px]">Hi there! This is my portfolio site, created in 2025. Learn more about this site in the <a href="/README">README</a> and check out some of my other work <a href="https://evakill.github.io">here</a>. Enjoy your wanderings!</p>}
+        {node.key == "" && <p className="text-base fixed bottom-50 left-15 w-[200px]">Hi there! This is my portfolio site, created in 2025. Learn more about this site in the <a href="https://github.com/evakill/evakillenbergv25/blob/main/README.md">README</a> and check out some of my other work <a href="https://evakill.github.io">here</a>. Enjoy your wanderings!</p>}
     </>
 
 }
